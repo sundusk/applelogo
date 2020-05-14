1caf107c2de02324e5c2a6c09aa6a8082e149e1b
sundusk <zhuyue346@gmail.com>
2020-05-14 16:55:03 +0800

    创建项目

--- AppleLogo/AppleLogo/ViewController.swift
+++ AppleLogo/AppleLogo/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
-
+import SnapKit
+import AuthenticationServices
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.yellow
+        
+        //  login
+        
+        
+        var appleIDButton = ASAuthorizationAppleIDButton.init(authorizationButtonType: .signIn, authorizationButtonStyle: .white)
+        
+        self.view.addSubview(appleIDButton)
+        appleIDButton.snp.makeConstraints { (make) in
+            make.center.equalTo(self.view)
+            make.height.equalTo(50)
+            make.width.equalTo(150)
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
